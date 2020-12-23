a0b060661fc14f9a0a89aef6255601c07f9f5772
nieisnuje <polina199925@gmail.com>
2020-12-23 17:51:56 +0300

    Add desc to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_133706) do
+ActiveRecord::Schema.define(version: 2020_12_23_144901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_12_06_133706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_url"
+    t.string "description"
   end
 
   create_table "favorites", force: :cascade do |t|
